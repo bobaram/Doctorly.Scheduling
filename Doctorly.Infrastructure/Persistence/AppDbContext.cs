acd3350ea8c73febc1f6cd9d9dea03f7f5f2384e
Omen Tinashe Tapfumaneyi <Omen@apstory.co.za>
2026-03-14 12:26:27 +0200

    Added AppDbContext with entity configurations

---
+++ Doctorly.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,25 @@
+using Doctorly.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Doctorly.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<CalendarEvent> CalendarEvents => Set<CalendarEvent>();
+    public DbSet<Attendee> Attendees => Set<Attendee>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        
+        modelBuilder.Entity<CalendarEvent>(builder =>
+        {
+            builder.OwnsOne(x => x.Duration);
+            builder.Property(x => x.RowVersion).IsRowVersion();
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
