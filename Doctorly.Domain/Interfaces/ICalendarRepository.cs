50ba1dadfb8ed08f9638bf77c3ab8621ca515202
Omen Tinashe Tapfumaneyi <Omen@apstory.co.za>
2026-03-14 12:32:12 +0200

    Added ICalendarRepository interface

---
+++ Doctorly.Domain/Interfaces/ICalendarRepository.cs
@@ -0,0 +1,14 @@
+using Doctorly.Domain.Entities;
+
+namespace Doctorly.Domain.Interfaces;
+
+public interface ICalendarRepository
+{
+    Task AddAsync(CalendarEvent calendarEvent, CancellationToken ct = default);
+    Task<CalendarEvent?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<IEnumerable<CalendarEvent>> ListAsync(DateTime from, DateTime to, CancellationToken ct = default);
+    Task<IEnumerable<CalendarEvent>> SearchAsync(string query, CancellationToken ct = default);
+    Task UpdateAsync(CalendarEvent calendarEvent, CancellationToken ct = default);
+    Task DeleteAsync(Guid id, CancellationToken ct = default);
+    Task SaveChangesAsync(CancellationToken ct = default);
+}
